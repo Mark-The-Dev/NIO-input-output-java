6439ae0808ee6e5dc62c872669804f2545f598c3
Mark <SwiftTactics@gmail.com>
2021-04-17 20:41:51 -0400

    added bufferReader, refactorer writer to use chained puts

--- src/NewNIO/Demo3.java
+++ src/NewNIO/Demo3.java
@@ -1,5 +1,6 @@
 package NewNIO;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -12,92 +13,41 @@ public class Demo3 {
             FileChannel binChannel = binFile.getChannel()) {
 
             ByteBuffer buffer = ByteBuffer.allocate(100);
+
             byte[] outputBytes = "Hello World!".getBytes();
-            buffer.put(outputBytes);
-            buffer.putInt(245);
-            buffer.putInt(-98745);
             byte[] outputBytes2 = "Nice to meet you".getBytes();
-            buffer.put(outputBytes2);
-            buffer.putInt(1000);
+            buffer.put(outputBytes).putInt(245).putInt(-98745).put(outputBytes2).putInt(1000);
             buffer.flip();
+
+//            byte[] outputBytes = "Hello World!".getBytes();
+//            buffer.put(outputBytes);
+//            buffer.putInt(245);
+//            buffer.putInt(-98745);
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes2);
+//            buffer.putInt(1000);
+//            buffer.flip();
+
             binChannel.write(buffer);
 
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
+
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte[] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte[] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("Int 3 = " + readBuffer.getInt());
 
 
-            // wraps byte array into buffer
-//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
-//            buffer.put(outputBytes);
-//
-//            buffer.flip();
-//
-//            int numBytes = binChannel.write(buffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-//            intBuffer.putInt(245);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            intBuffer.flip();
-//            intBuffer.putInt(-98745);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            // IO way
-////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-////            byte[] b = new byte[outputBytes.length];
-////            ra.read(b);
-////            System.out.println(new String(b));
-////
-////            long int1 = ra.readInt();
-////            long int2 = ra.readInt();
-////            System.out.println(int1);
-////            System.out.println(int2);
-//
-//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-//            FileChannel channel = ra.getChannel();
-//            // change to test buffer read working, edits the string without flip.
-//            outputBytes[0] = 'a';
-//            outputBytes[1] = 'b';
-//            buffer.flip();
-//            long numBytesRead = channel.read(buffer);
-//            if(buffer.hasArray()){
-//                //System.out.println("byteBuffer = " + new String(buffer.array()));
-//                System.out.println("byteBuffer = " + new String(outputBytes));
-//            }
-//
-//            // Absolute Read
-//
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            System.out.println(intBuffer.getInt(0));
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            intBuffer.flip();
-//
-//            // absolute read does not change the buffers position.
-//            System.out.println(intBuffer.getInt(0));
-//            System.out.println(intBuffer.getInt());
-//
-//
-//
-//            // Relative read
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-////            intBuffer.flip();
-////            numBytesRead = channel.read(intBuffer);
-////            intBuffer.flip();
-////            System.out.println(intBuffer.getInt());
-//
-//
-//            channel.close();
-//            ra.close();
 
-            //System.out.println("outputBytes = " + new String(outputBytes));
 
 
 
